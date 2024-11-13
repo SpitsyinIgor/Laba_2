ed835b5584d107757b0dc6ef82947abddb2bd25f
SpitsyinIgor <1overflash1@gmail.com>
2024-11-14 01:58:53 +0300

    Update Utilities.java
    Добавлена функция getPrecedence

--- src/main/java/org/example/Utilities.java
+++ src/main/java/org/example/Utilities.java
@@ -9,5 +9,20 @@ public class Utilities {
             return false;
         }
     }
+    
+    public static int getPrecedence(String operator) {
+        switch (operator) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            case "^":
+                return 3;
+            default:
+                return 0;
+        }
+    }
 
 }
