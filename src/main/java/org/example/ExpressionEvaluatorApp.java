c1212e7c5fdec62ddc3413c41fcc82e678aa4386
SpitsyinIgor <1overflash1@gmail.com>
2024-11-13 23:49:47 +0300

    Update ExpressionEvaluatorApp.java
    Создана функция main

--- src/main/java/org/example/ExpressionEvaluatorApp.java
+++ src/main/java/org/example/ExpressionEvaluatorApp.java
@@ -1,4 +1,23 @@
-package org.example;
+package org.example.ExpressionEvaluator;
 
+import java.util.*;
 public class ExpressionEvaluatorApp {
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите выражение: ");
+        String expression = scanner.nextLine();
+
+        Map<String, Double> variables = new HashMap<>();
+
+        try {
+            double result = evaluateExpression(expression, variables);
+            System.out.println("Результат: " + result);
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+
+        return operandStack.pop();
+    }
 }
