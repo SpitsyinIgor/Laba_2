d49751294066c6594814a386eb0e5ffd6261c152
SpitsyinIgor <1overflash1@gmail.com>
2024-11-14 01:42:33 +0300

    Update Utilities.java
    Добавлен класс Utilities с функцией isNumeric

--- src/main/java/org/example/Utilities.java
+++ src/main/java/org/example/Utilities.java
@@ -1,4 +1,13 @@
-package org.example;
+package org.example.ExpressionEvaluator;
 
 public class Utilities {
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
