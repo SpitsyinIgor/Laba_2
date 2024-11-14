f37a1c490844a43edcef985839d669785ac72b79
SpitsyinIgor <1overflash1@gmail.com>
2024-11-14 10:36:30 +0300

    Update UtilitiesTest.java
    Добавлен тест для проверки, является ли строка числом

--- src/test/java/org/example/UtilitiesTest.java
+++ src/test/java/org/example/UtilitiesTest.java
@@ -1,4 +1,22 @@
-package org.example;
+package ExpressionEvaluatorTest;
 
+
+import org.example.ExpressionEvaluator.Utilities;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 public class UtilitiesTest {
+    @Test
+    public void testIsNumeric() {
+        // Тест для проверки, является ли строка числом
+
+        assertTrue(Utilities.isNumeric("123"));
+        assertTrue(Utilities.isNumeric("12.34"));
+        assertFalse(Utilities.isNumeric("abc"));
+        assertFalse(Utilities.isNumeric("1a2b3c"));
+    }
+
+   
 }
